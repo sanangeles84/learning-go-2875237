c05547aa88459db13cb3a2c743b5a01dc4669df8
Angelo Santillo <angelo.santillo@gmail.com>
2023-05-18 19:35:56 +0200

    .

--- calculator/main.go
+++ calculator/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Println("A simple calculator")
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Value 1: ")
+	str1, _ := reader.ReadString('\n')
+	float1, err := strconv.ParseFloat(strings.TrimSpace(str1), 64)
+	if err != nil {
+		fmt.Println(err)
+		panic("Value 1 must be a number")
+	}
+
+	fmt.Print("Value 2: ")
+	str2, _ := reader.ReadString('\n')
+	float2, err := strconv.ParseFloat(strings.TrimSpace(str2), 64)
+	if err != nil {
+		fmt.Println(err)
+		panic("Value 1 must be a number")
+	}
+
+	float3 := float1 + float2
+	float3 = math.Round(float3*100) / 100
+	fmt.Printf("The sum of %v and %v is %v", float1, float2, float3)
 }
