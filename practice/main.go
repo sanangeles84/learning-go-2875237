02c4fdf12fe38de548ef502c7d3337c32a94e15a
Angelo Santillo <angelo.santillo@gmail.com>
2023-05-19 15:41:37 +0200

    .

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
-const url = "http://services.explorecalifornia.org/json/tours.php"
+// const url = "http://services.explorecalifornia.org/json/tours.php"
+
+const url = "https://raw.githubusercontent.com/sanangeles84/learning-go-2875237/main/tours.json"
 
 func main() {
-	fmt.Println("Network requests")
+
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Response type: %T\n", resp)
+
+	defer resp.Body.Close()
+
+	fmt.Println(resp.Status)
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	content := string(bytes)
+	fmt.Print(content)
 }
