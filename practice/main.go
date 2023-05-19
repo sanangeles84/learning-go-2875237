ccc32f9d1b3f6922d82770fa1ee7ad4678659abe
Angelo Santillo <angelo.santillo@gmail.com>
2023-05-19 13:27:56 +0200

    .

--- practice/main.go
+++ practice/main.go
@@ -5,14 +5,32 @@ import (
 )
 
 func main() {
-	poodle := Dog{"Poodle", 10}
+	poodle := Dog{"Poodle", 10, "Woof!"}
 	fmt.Println(poodle)
 	fmt.Printf("%+v\n", poodle)
 	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+
+	poodle.Speak()
+	poodle.Sound = "Arf!"
+	poodle.Speak()
+	poodle.SpeakThreetimes()
+	poodle.SpeakThreetimes()
 }
 
 // Dog is a struct
 type Dog struct {
 	Breed  string
 	Weight int
+	Sound  string
+}
+
+// Speak is how the dog speaks
+func (d Dog) Speak() {
+	fmt.Println(d.Sound)
+}
+
+// SpeakThreetimes is how the dog speaks loudly
+func (d Dog) SpeakThreetimes() {
+	d.Sound = fmt.Sprintf("%v %v %v", d.Sound, d.Sound, d.Sound)
+	fmt.Println(d.Sound)
 }
