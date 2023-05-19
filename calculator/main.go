d098ea4b8b99b780665626de86738a2d08405c28
Angelo Santillo <angelo.santillo@gmail.com>
2023-05-19 13:48:46 +0200

    .

--- calculator/main.go
+++ calculator/main.go
@@ -9,27 +9,62 @@ import (
 	"strings"
 )
 
+var reader = bufio.NewReader(os.Stdin)
+
 func main() {
-	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Value 1: ")
-	input1, _ := reader.ReadString('\n')
-	float1, err := strconv.ParseFloat(strings.TrimSpace(input1), 64)
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 1 must be a number")
+	float1 := getValue("1")
+	float2 := getValue("2")
+
+	var result float64
+
+	switch operation := getOperation(); operation {
+	case "+":
+		result = addValues(float1, float2)
+	case "-":
+		result = substractValues(float1, float2)
+	case "*":
+		result = multiplyValues(float1, float2)
+	case "/":
+		result = divideValues(float1, float2)
+	default:
+		panic(" Operation must be one of these + - * /")
 	}
 
-	fmt.Print("Value 2: ")
-	input2, _ := reader.ReadString('\n')
-	float2, err := strconv.ParseFloat(strings.TrimSpace(input2), 64)
+	result = math.Round(result*100) / 100
+	fmt.Printf("The result is %v\n\n", result)
+
+}
+
+func getValue(valueNumber string) float64 {
+	fmt.Printf("Value %v: ", valueNumber)
+	input, _ := reader.ReadString('\n')
+	floatIn, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
 	if err != nil {
 		fmt.Println(err)
-		panic("Value 2 must be a number")
+		panic(fmt.Sprintf("Value %v must be a number", valueNumber))
 	}
+	return floatIn
+}
 
-	sum := float1 + float2
-	sum = math.Round(sum*100) / 100
-	fmt.Printf("The sum of %v and %v is %v\n\n", float1, float2, sum)
+func getOperation() string {
+	fmt.Print("Select an operation (+ - * /): ")
+	opInput, _ := reader.ReadString('\n')
+	return strings.TrimSpace(opInput)
+}
 
+func addValues(value1, value2 float64) float64 {
+	return value1 + value2
+}
+
+func substractValues(value1, value2 float64) float64 {
+	return value1 - value2
+}
+
+func multiplyValues(value1, value2 float64) float64 {
+	return value1 * value2
+}
+
+func divideValues(value1, value2 float64) float64 {
+	return value1 / value2
 }
